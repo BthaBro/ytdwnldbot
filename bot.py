0037b7c2d0dae8f9d854cce214cd5940cddd3c3c
Abror Khamrokhujaev <abrorkhodza@mail.ru>
2020-01-25 17:35:10 +0500

    Changing video settings

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, ConversationHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, Chat, ChatAction
 import pytube
 from io import BytesIO
 import logging
@@ -19,24 +19,21 @@ def start(update, context): # start command
 
 
 def video(update, context): # video
-    reply_keyboard = [['720p', '480p', '360p']] # reply keyboard for quality
+    reply_keyboard = [['720p','360p']] # reply keyboard for quality
 
     update.message.reply_text('Please choose the quality of video\nOr send /cancel to cancel', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
     return SETTINGS
 
-itg = '135'
-
-
+itg = 0
 def video_set(update, context): # video settings
     user = update.message.from_user
     quality = update.message.text
+    global itg
     if quality == '720p':
         itg = '22'
-    elif quality == '480p':
-        itg = '135'
     elif quality == '360p':
-        itg = '134'
+        itg = '18'
     logger.info("User  %s chose %s", user.first_name, update.message.text)
     update.message.reply_text('Now send me the link please', reply_markup = ReplyKeyboardRemove())
 
@@ -47,21 +44,21 @@ def video_send(update, context): # sending video
     link = update.message.text
     user = update.message.from_user
     logger.info("User %s sent %s", user.first_name, update.message.text)
-    if 'youtu' not in link:
-        context.bot.send_message(chat_id = update.effective_chat.id, text = "It's not a valid link.")
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "Accessing youtube link...")
+    try:
+        video = pytube.YouTube(link).streams.get_by_itag(itg)
+    except:
+        context.bot.send_message(chat_id = update.effective_chat.id, text = "Oops, something went wrong.\nMaybe there's no available with that quality.")
+        return ConversationHandler.END
     else:
-        try:
-            yt = pytube.YouTube(link)
-            stream = yt.streams.get_by_itag(itg)
-        except Exception as e:
-            print(e)
-            context.bot.send_message(chat_id = update.effective_chat.id, text = "Oops, seems like there was an error")
-        else:
-            buf = stream.stream_to_buffer()
-            print(buf)
-            buf.seek(0)
-            context.bot.send_video(chat_id = update.effective_chat.id, video = buf)
-
+        title = video.title
+        context.bot.send_message(chat_id = update.effective_chat.id, text = "Fetching: {}...".format(title))
+        buf = video.stream_to_buffer()
+        print(buf)
+        buf.seek(0)
+        context.bot.send_chat_action(chat_id = update.effective_chat.id, action=ChatAction.UPLOAD_VIDEO)
+        context.bot.send_video(chat_id = update.effective_chat.id, video = buf)
+        context.bot.send_message(chat_id = update.effective_chat.id, text = "If bot has not sent you a video, file size of video might be more than 50 MB")
     return ConversationHandler.END
 
 
@@ -93,8 +90,7 @@ def audio_send(update, context):
         try:
             yt = pytube.YouTube(link)
             stream = yt.streams.filter(only_audio=True).first()
-        except Exception as e:
-            logger.info(e)
+        except:
             context.bot.send_message(chat_id = update.effective_chat.id, text = "Oops, seems like there was an error")
         else:
             buf = stream.stream_to_buffer()
@@ -111,7 +107,7 @@ def help(update, context):
 
 
 def main(): # main fuction
-    updater = Updater(token='1072871384:AAFGbIjIt1OO4rnbMDqIIn1Sco3sZhjVW_8', use_context=True)
+    updater = Updater(token='983360652:AAHBOs8EyivOVeOYgt7nM7LSxzY-x7J5nKM', use_context=True, request_kwargs={'read_timeout': 6, 'connect_timeout': 7})
     dp = updater.dispatcher
     # start command
     dp.add_handler(CommandHandler('start', start))
@@ -119,7 +115,7 @@ def main(): # main fuction
         entry_points=[CommandHandler('video', video)],
 
         states = {
-            SETTINGS : [MessageHandler(Filters.regex('^(720p|480p|360p)$'), video_set)],
+            SETTINGS : [MessageHandler(Filters.regex('^(720p|360p)$'), video_set)],
             VIDEO : [MessageHandler(Filters.text, video_send)]
         },
 
